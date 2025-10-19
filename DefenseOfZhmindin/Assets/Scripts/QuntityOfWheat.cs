f710ca1cb18f67fbd0bc70fa245691d4cd3a731b
DartPingvin <einemauss@mail.ru>
2025-10-19 21:55:06 +0300

    1.0 PreWaveUpdate

--- DefenseOfZhmindin/Assets/Scripts/QuntityOfWheat.cs
+++ DefenseOfZhmindin/Assets/Scripts/QuntityOfWheat.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Timers;
 using TMPro;
 using UnityEngine;
@@ -7,6 +8,7 @@ public class QuntityOfWheat : MonoBehaviour
 {
     [SerializeField] private Button BuyPeasant;
     [SerializeField] private Button BuyKnight;
+    [SerializeField] private Button StartNewWave;
     [SerializeField] private TextMeshProUGUI WheatMesh;
     [SerializeField] private int StartWheat;
     [SerializeField] private TextMeshProUGUI PeasantNum;
@@ -15,40 +17,118 @@ public class QuntityOfWheat : MonoBehaviour
     [SerializeField] private TextMeshProUGUI KnightNum;
     [SerializeField] private int StartKnight;
     [SerializeField] private int KnightCost;
-    [SerializeField] private TextMeshProUGUI TimerCount;
-    [SerializeField] private float TimerStart;
+    [SerializeField] private TextMeshProUGUI HarvestCount;
+    [SerializeField] private float TimerStartHarvest;
+    [SerializeField] private TextMeshProUGUI ConsumptionCount;
+    [SerializeField] private float TimerStartConsumption;
+    [SerializeField] private TextMeshProUGUI ConsumptionText;
+    [SerializeField] private TextMeshProUGUI ProductionText;
+    [SerializeField] private TextMeshProUGUI PopulationText;
+    [SerializeField] private TextMeshProUGUI TimeBeforeWave;
+    [SerializeField] private int WaveCount;
+    [SerializeField] private TextMeshProUGUI WaveNum;
+    [SerializeField] private int RewardNum;
+    [SerializeField] private TextMeshProUGUI Reward;
+    [SerializeField] private TextMeshProUGUI WaveEnemies;
+    [SerializeField] private int Enemies;
+    
 
-    private void Awake()
+
+    void Awake()
     {
         UpdateUI();
         BuyPeasant.onClick.AddListener(Peasant);
         BuyKnight.onClick.AddListener(Knight);
+        StartNewWave.onClick.AddListener(Wave);
     }
 
-    private void Update()
+    void Update() //Prodaction-ConsumptionTimers
     {
         int WheatNum = StartWheat;
-        float CurentTime = TimerStart - Time.time;
-        TimerCount.text = Mathf.Round(CurentTime).ToString();
-        if (CurentTime <= 0)
+        float HarvestTime = TimerStartHarvest - Time.time;
+        HarvestCount.text = Mathf.Round(HarvestTime).ToString();
+        if (HarvestTime <= 0)
         {
             WheatNum = WheatNum + StartPeasant * 2;
-            CurentTime = 15;
-            TimerStart = 15;
+            TimerStartHarvest = 15 + Time.time;
+            StartWheat = WheatNum;
+        }
+        float ConsumptionTime = TimerStartConsumption - Time.time;
+        ConsumptionCount.text = Mathf.Round(ConsumptionTime).ToString();
+        if (ConsumptionTime <= 0)
+        {
+            WheatNum = WheatNum - (StartPeasant + StartKnight*3);
+            TimerStartConsumption = 20 + Time.time;
+            
+            if (WheatNum < 0)
+            {
+                StartKnight = StartKnight + Math.DivRem(WheatNum, 3, out int result);
+                StartPeasant = StartPeasant + result;
+                if (StartKnight < 0)
+                {
+                    StartPeasant = StartPeasant + StartKnight * 3;
+                    WheatNum = 0;
+                    StartKnight = 0;
+                }
+                else
+                {
+                    WheatNum = 0;
+                }
+            }
+            StartWheat = WheatNum;
         }
         WheatMesh.text = WheatNum.ToString();
+        UpdateUI();
 
     }
     private void Peasant()
     {
+        if (StartWheat >= PeasantCost)
+        {
+            int WheatNum = StartWheat;
+            int Peasantnum = StartPeasant;
+            int Cost = PeasantCost;
+            WheatNum = WheatNum - Cost;
+            Peasantnum = Peasantnum + 1;
+            StartWheat = WheatNum;
+            StartPeasant = Peasantnum;
+            WheatMesh.text = StartWheat.ToString();
+            PeasantNum.text = StartPeasant.ToString();
+        }
         
     }
-
     private void Knight()
     {
-        UpdateUI();
+        if (StartWheat >= KnightCost)
+        {
+            int WheatNum = StartWheat;
+            int Knightnum = StartKnight;
+            int Cost = KnightCost;
+            WheatNum = WheatNum - Cost;
+            Knightnum = Knightnum + 1;
+            StartWheat = WheatNum;
+            StartKnight = Knightnum;
+            WheatMesh.text = StartWheat.ToString();
+            KnightNum.text = StartKnight.ToString();
+            UpdateUI();
+        }
+
     }
-    private void UpdateUI()
+    void UpdateUI()
+    {
+        WheatMesh.text = StartWheat.ToString();
+        PeasantNum.text = StartPeasant.ToString();
+        KnightNum.text = StartKnight.ToString();
+        int Consumption = (StartPeasant + StartKnight * 3);
+        ConsumptionText.text = Consumption.ToString();
+        int Production = (StartPeasant * 2);
+        ProductionText.text = Production.ToString();
+        int Population = (StartPeasant + StartKnight);
+        PopulationText.text = Population.ToString();
+        
+
+    }
+    private void Wave()
     {
         
     }
