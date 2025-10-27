0d775c2dd3c208248f7cbd74a4debb5019654274
DartPingvin <einemauss@mail.ru>
2025-10-27 16:25:22 +0300

    PauseMenu and Gold Update 1.75

--- DefenseOfZhmindin/Assets/Scripts/PauseMiniGame.cs
+++ DefenseOfZhmindin/Assets/Scripts/PauseMiniGame.cs
@@ -1,16 +1,34 @@
+using NUnit.Framework.Internal.Execution;
 using System;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Windows;
 
 public class PauseMiniGame : MonoBehaviour
 {
-    [SerializeField] private string[] TestIngredients;
     [SerializeField] private List<Item> ItemData;
     [SerializeField] private TextMeshProUGUI Output;
+    [SerializeField] private TMP_InputField ING1;
+    [SerializeField] private Button create;
+    private string[] TestIngredients = new string[4];
+
+
+
     private void Awake()
     {
+        create.onClick.AddListener(Create);
+    }
+    public void Create()
+    {
+        string ING2 = ING1.ToString();
+        List<string> Ingredients = new List<string> (ING2.Split(' '));
+        TestIngredients[0] = Ingredients[0];
+        TestIngredients[1] = Ingredients[1];
+        TestIngredients[2] = Ingredients[2];
+        TestIngredients[3] = Ingredients[3];
         bool succes = TryMakeItem(TestIngredients, out string ItemName);
         if (succes)
         {
@@ -29,13 +47,30 @@ public class PauseMiniGame : MonoBehaviour
             string[] ItemIngridients = item.Ingredients;
             if (TestIngredients.Length == ItemIngridients.Length)
             {
-                for (int i = 0; i < TestIngredients.Length; i++)
+                if (ItemIngridients[0] == TestIngredients[0])
                 {
-                    if (TestIngredients[i] == ItemIngridients[i])
+                    if (TestIngredients[1] == ItemIngridients[1])
                     {
-                        ItemName = item.Name;
-                        return true;
+                        if (TestIngredients[2] == ItemIngridients[2])
+                        {
+                            if (TestIngredients[3] == ItemIngridients[3])
+                            {
+                                ItemName = item.Name;
+                                return true;
+                            }
+                            Debug.Log($"Searching for {ItemIngridients[3]} 4");
+                            Debug.Log($"Was Given {TestIngredients[3]} 4");
+                        }
+                        Debug.Log($"Searching for {ItemIngridients[2]} 3");
+                        Debug.Log($"Was Given {TestIngredients[2]} 3");
                     }
+                    Debug.Log($"Searching for {ItemIngridients[1]} 2");
+                    Debug.Log($"Was Given {TestIngredients[1]} 2");
+                }
+                else
+                {
+                    Debug.Log($"Searching for {ItemIngridients[0]} - {ItemIngridients[0].GetType()}");
+                    Debug.Log($"Was Given {TestIngredients[0]} - {TestIngredients[0].GetType()}");
                 }
             }
         }
